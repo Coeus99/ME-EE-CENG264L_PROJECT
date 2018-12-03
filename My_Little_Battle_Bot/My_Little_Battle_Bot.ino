0c10e29e700ea04fd3318204b435a97f58c55ba7
Grosz, Logan <logan.grosz@mines.sdsmt.edu>
2018-12-02 19:46:08 -0700

    locomotion should be working, however, it is untested

--- My_Little_Battle_Bot/My_Little_Battle_Bot.ino
+++ My_Little_Battle_Bot/My_Little_Battle_Bot.ino
@@ -19,12 +19,9 @@
 #include <Adafruit_PWMServoDriver.h>
 #define SERVOMIN 200//don't remember what was said in class
 #define SERVOMAX 400//don't remember what was said in class
-const uint8_t min_position = 0;//don't remember what was said in class
-const uint8_t max_position = 100;//don't remember what was said in class, should be multiple of 10
+const uint8_t min_position = 60;
+const uint8_t max_position = 90;
 Adafruit_PWMServoDriver servo_driver = Adafruit_PWMServoDriver();
-
-//Prototypes
-void set_motors( uint8_t left_speed, bool left_reverse, uint8_t right_speed, bool right_reverse);
 
 //Motors
 /**
@@ -49,6 +46,27 @@ USB usb;
 BTD btd( &usb );
 //Create DS4 instance from BT instance
 PS4BT ps4( &btd );
+
+/**
+ * Prototypes
+ */
+void locomotion();
+void weapons();
+void panels();
+
+/**
+ * Flags
+ */
+bool left_idle = true;
+bool right_idle =true;
+bool left_max = false;
+bool right_max = false;
+
+/**
+ * Previous Values
+ */
+uint8_t prev_right_y = 0;
+uint8_t prev_left_y = 0;
 
 void setup()
 {
@@ -78,5 +96,146 @@ void loop()
   if( ps4.connected() )
   {
     //check all applicable controls
+    locomotion();
+    weapons();
+    panels();
   }
+}
+
+void locomotion()
+{
+  //get controller data
+  uint8_t right_y,left_y,right_speed,left_speed;
+  bool left_reverse, right_reverse;
+
+  left_y = ps4.getAnalogHat(LeftHatY);
+  right_y = ps4.getAnalogHat(RightHatY);
+
+  //left side
+  if(117 < left_y < 137)
+  {
+    if(!left_idle)
+    {
+      left_idle = true;
+      left_speed = 0;
+      front_left->run(RELEASE);
+      back_left->run(RELEASE);
+    }
+  }
+  else if(left_y > 250)
+  {
+    if(!left_max)
+    {
+      left_max = true;
+      left_speed = 255;
+      front_left->setSpeed(left_speed);
+      back_left->setSpeed(left_speed);
+      if (left_y > 250)
+      {
+        left_reverse = false;
+        front_left->run(FORWARD);
+        back_left->run(FORWARD);
+      }
+      else
+      {
+        left_reverse = true;
+        front_left->run(BACKWARD);
+        back_left->run(BACKWARD);
+      }
+    }
+  }
+  else if(abs((int)left_y - (int)prev_left_y) > 10)
+  {
+    if ( left_y > 137 )
+    {
+      //Left side is in reverse
+      left_speed = map( left_y, 138, 255, 0, 255 );
+      left_reverse = true;
+    }
+    else if ( left_y < 117 )
+    {
+      //Left side is forward
+      left_speed = map( left_y, 116, 0, 0, 255 );
+      left_reverse = false;
+    }
+  }
+  prev_left_y = left_y;
+
+  //right side
+  if(117 < right_y < 137)
+  {
+    if(!right_idle)
+    {
+      right_idle = true;
+      right_speed = 0;
+      front_right->run(RELEASE);
+      back_right->run(RELEASE);
+    }
+  }
+  else if(right_y > 250)
+  {
+    if(!right_max)
+    {
+      right_max = true;
+      right_speed = 255;
+      front_right->setSpeed(right_speed);
+      back_right->setSpeed(right_speed);
+      if (right_y > 250)
+      {
+        right_reverse = false;
+        front_right->run(FORWARD);
+        back_right->run(FORWARD);
+      }
+      else
+      {
+        right_reverse = true;
+        front_right->run(BACKWARD);
+        back_right->run(BACKWARD);
+      }
+    }
+  }
+  else if(abs((int)right_y - (int)prev_right_y) > 10)
+  {
+    if ( right_y > 137 )
+    {
+      //right side is in reverse
+      right_speed = map( right_y, 138, 255, 0, 255 );
+      right_reverse = true;
+    }
+    else if ( right_y < 117 )
+    {
+      //right side is forward
+      right_speed = map( right_y, 116, 0, 0, 255 );
+      right_reverse = false;
+    }
+  }
+  prev_right_y = right_y;
+}
+
+void weapons()
+{
+  //if square -> shoot left cannon
+  if(ps4.getButtonClick(SQUARE))
+  {
+    //to fire -> 60-90
+    //to load -> 90-60
+  }
+  //if circle -> shoot left cannon
+  if(ps4.getButtonClick(CIRCLE))
+  {
+    //to fire -> 60-90
+    //to load -> 90-60
+  }
+  //if triangle -> shoot laser for 1 second (use millis() timers), use flag
+  if(ps4.getButtonClick(TRIANGLE))
+  {
+    
+  }
+  //if right_trigger -> laser on
+}
+
+void panels()
+{
+  //if panels is hit decrease speed
+  //if panels is hit twice cut speed
 }
